32bbf84ce4c8427e4d1f5b3ff18efc992d0f0648
Jason van Willigen <jasonvw@live.nl>
2019-08-28 13:52:43 +0200

    medewerker knoppen en artiest aanmelding toegevoegd

--- artiesten.php
+++ artiesten.php
@@ -70,9 +70,29 @@
   <!--end artiesten cards-->
 
   <!--start modal artiest toevoegen-->
-    <div class="modal fade" id="ArtistAdd" tabindex="-1" role="dialog" aria-hidden="true">
-    
-    </div>
+  <div class="modal fade" id="ArtistAdd" tabindex="-1" role="dialog" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">Artiest toevoegen</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <form method="post" action="ariesten.php">
+                <div class="modal-body">
+                  <div class="form-group">
+                    <label for="artiestid">Artiest id</label>
+                    <input type="number" name="title" class="form-control" required> 
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Sluiten</button>
+                  <input type="submit" class="btn btn-success" name="submit" value="Artiest toevoegen">
+                </div>
+              </form>  
+            </div>
+          </div>
   <!--end modal artiest toevoegen-->
 
   <!--start modal artiest aanpassen-->
